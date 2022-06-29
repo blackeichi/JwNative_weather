0ed2c1a5d36a78faa60a336a8cd6855cf3f0f11f
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-06-29 13:05:15 +0900

    20220629

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <View style={styles.weather}>
+        <Text>27°</Text>
+        <Text>Sunny</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#5AD18F",
+  },
+  city: {
+    flex: 1,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
   },
 });
